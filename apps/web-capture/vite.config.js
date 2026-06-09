d7821b1518afba169eb85ebb0d5e59012d7f9e1a
DigiArch 424 <159387328+digi4arch424@users.noreply.github.com>
2026-06-09 20:34:00 +1000

    Change base path in Vite config to './'

--- apps/web-capture/vite.config.js
+++ apps/web-capture/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/spatial-recon-game/',
+  base: './',
   server: {
     port: 5173,
     https: false
